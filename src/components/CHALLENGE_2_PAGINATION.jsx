343474f5909ecdc3f6de22e05d238926a339303c
mohsen ansari <mohsen.ansari.bim@gmail.com>
2024-12-26 01:32:25 +0330

    second challenge, pagination, part 2(backend base pagination)

--- src/components/CHALLENGE_2_PAGINATION.jsx
+++ src/components/CHALLENGE_2_PAGINATION.jsx
@@ -5,37 +5,47 @@ import "../assets/css/CHALLENGE_2.css";
 const CHALLENGE_2_PAGINATION = () => {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+
+  const RoundedValueComponent = (value) => {
+    const roundedValue = Math.round(value);
+    return roundedValue;
+  };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await axios("https://dummyjson.com/products?limit=100");
-        if (res.data && res.data.products) {
-          setData(res.data.products);
-        }
+        const res = await axios(
+          ` https://dummyjson.com/products?limit=19&skip=${
+            page * 19 - 19
+          }&select=thumbnail,title`
+        );
+
+        setData(res.data.products);
+        setTotalPages(RoundedValueComponent(res.data.total / 18));
       } catch (err) {
         console.log(err);
       }
     };
     fetchData();
-  }, []);
+  }, [page]);
 
   const handlePrevious = () => {
     if (page > 1) {
       setPage(page - 1);
     }
   };
-
   const handleNext = () => {
-    if (page < data.length / 10) {
+    if (page < totalPages) {
       setPage(page + 1);
     }
   };
+  console.log(totalPages, page);
 
   return (
     <>
       <div className="container">
-        {data.slice(page * 10 - 10, page * 10).map((d) => {
+        {data.map((d) => {
           return (
             <span key={d.id}>
               <p className="title">{d.title}</p>
@@ -51,7 +61,7 @@ const CHALLENGE_2_PAGINATION = () => {
         >
           ◀
         </span>
-        {[...Array(data?.length / 10)].map((_, i) => {
+        {[...Array(totalPages)].map((_, i) => {
           return (
             <span
               style={{ background: page === i + 1 ? "green" : "" }}
@@ -63,7 +73,7 @@ const CHALLENGE_2_PAGINATION = () => {
           );
         })}
         <span
-          style={{ opacity: page < data.length / 10 ? "1" : "0" }}
+          style={{ opacity: page < totalPages ? "1" : "0" }}
           onClick={handleNext}
         >
           ▶
